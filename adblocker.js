a2389209aa207eab89e8a1e8563c452b65de0c84
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-06-04 12:35:05 +0100

    Beautify adblocker.js

--- adblocker.js
+++ adblocker.js
@@ -1,21 +1,23 @@
-(function(what){
+(function(what) {
   var rm = {
-    list(elems){
-      Array.from(elems).forEach(v=>v.remove())
+    list(elems) {
+      Array.from(elems).forEach(v => v.remove())
     }
-    cls(name){
+    cls(name) {
       rm.list(document.getElementsByClassName(name))
     }
-    selector(selector){
+    selector(selector) {
       rm.list(document.querySelectorAll(selector))
     }
-}
- 
- for(let name in what){
-  let args = what[name];
-  for(let i=0;i<args.length;i++){
-    let arg = args[i];
-    rm[i](arg);
   }
-}
-})({cls: ['adsbygoogle','mod_ad_container']})
+
+  for (let name in what) {
+    let args = what[name];
+    for (let i = 0; i < args.length; i++) {
+      let arg = args[i];
+      rm[i](arg);
+    }
+  }
+})({
+  cls: ['adsbygoogle', 'mod_ad_container']
+})
