21adc1c7bf49c5bca930be9b6c42a75ebcc257df
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-07-29 21:54:15 +0100

    Remove as much whitespace as possible

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 import re
 
-def make_bookmarklet(src: str):
-  s = re.sub(r'\n\s*', '', src)
+def make_bookmarklet(s: str):
+  s = s.replace("\n", " ")
+  s = re.sub(r"""(?<![\w\$])\s*|\s*(?![\w\$])""", '', s)
   s = 'javascript:' + s
   return s
 
