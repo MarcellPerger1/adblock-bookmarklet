b9696a2299bf123d9264550df5ca686fbf3fa310
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-10-11 21:27:00 +0100

    Don't remove whitespace before/after quotes
    This is to fix the class=... selectors. I should really use a proper js minifier...

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import re
 
 def make_bookmarklet(s: str):
   s = s.replace("\n", " ")
-  s = re.sub(r"""(?<![\w\$])\s*|\s*(?![\w\$])""", '', s)
+  s = re.sub(r"""(?<![\w\$"'])\s*|\s*(?![\w\$"'])""", '', s)
   s = 'javascript:' + s
   return s
 
