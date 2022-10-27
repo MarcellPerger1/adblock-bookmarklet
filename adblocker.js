77411d32a8d118cdd5d49399af274a695587c868
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-10-27 14:36:20 +0100

    Add new `func` filter

--- adblocker.js
+++ adblocker.js
@@ -8,6 +8,16 @@
     },
     selector(selector) {
       rm.list(document.querySelectorAll(selector))
+    },
+    func({func, selector=null}) {
+      let elems = selector == null 
+        ? document.getElementsByClassName("*") 
+        : document.querySelectorAll(selector);
+      for (let elem of elems) {
+        if (func(elem)) {
+          elem.remove();
+        }
+      }
     }
   };
 
@@ -20,5 +30,18 @@
   }
 })({
   cls: ['adsbygoogle', 'mod_ad_container', 'brn-ads-box','gpt-ad','ad-box','top-ads-container', 'adthrive-ad'],
-  selector: ['[aria-label="advertisement"]', '[class*="-ad "], [class*="-ad-"], [class$="-ad"], [class^="ad-"]']
+  selector: ['[aria-label="advertisement"]', '[class*="-ad "], [class*="-ad-"], [class$="-ad"], [class^="ad-"]'],
+  /** @type {{selector: string?, func: (elem: Element) => any}[]} */
+  func: [
+    {
+      selector: '[class*="ad"]', 
+      func(elem) {
+        for (const clsname of elem.classList) {
+          if (clsname.startsWith("ad") || /[-_\s]ad(?:vertisement)?$/.test(clsname)) {
+            return true;
+          }
+        }
+      }
+    }
+  ],
 })
