94e5c16e96ed0cc19a51024d0d358b55dd114196
Marcell Perger <marcell.perger.1@gmail.com>
2026-06-18 12:50:46 +0100

    refactor: Extract functions to prepare for scanning subdocuments
    TODO: will need new mutation observers for each not-completely-ad visible iframe

--- src/adblocker.js
+++ src/adblocker.js
@@ -1,115 +1,120 @@
 /* eslint-env browser, es2021 */
-/** @copyright (c) 2022 Marcell Perger @license MIT */(function () {
-  var lastRun = -1e9;
-  function block() {
-    // run every 100ms at most, but clamp other side to prevent timezone weirdness
-    if(lastRun < Date.now() && Date.now() < lastRun + 100) return;
-    lastRun = Date.now();
-    (function (what) {
-      function shouldIgnore(elem) {
-        for (let s of what.ignore?.selector ?? []) {
-          if (elem.matches(s)) {
-            return true;
-          }
-        }
-        for (let f of what.ignore?.func ?? []) {
-          if (f(elem)) {
-            return true;
-          }
-        }
-        return false;
-      }
-      function isContainerElem(
-        /** @type {HTMLElement} */ elem,
-      ) /** @type {boolean} */ {
-        // .tagName returns UPPERCASE for some reason
-        return ["DIV", "SPAN"].includes(elem.tagName);
-      }
-      var rm = {
-        elem(/** @type {HTMLElement} */ elem) {
-          if (!shouldIgnore(elem)) {
-            removedElems.add([elem, elem.parentElement]);
-            // Anti-adblock stuff might overwrite their own prototype
-            // This isn't perfect by any means but it's good enough.
-            HTMLElement.prototype.remove.call(elem);
-          }
-        },
-        list(/** @type {HTMLElement[]} */ elems) {
-          Array.from(elems).forEach((v) => rm.elem(v));
-        },
-        cls(/**@type {string} */ name) {
-          rm.list(document.getElementsByClassName(name));
-        },
-        selector(/** @type {string} */ selector) {
-          rm.list(document.querySelectorAll(selector));
-        },
-        func({ func, selector = null }) {
-          let elems =
-            selector == null
-              ? document.getElementsByClassName("*")
-              : document.querySelectorAll(selector);
-          for (let elem of elems) {
-            if (func(elem)) {
-              rm.elem(elem);
-            }
-          }
-        },
-      };
-      var /** @type {Set<[HTMLElement, HTMLElement]>} */ removedElems =
-          new Set();
-      var handledElems /** @type {Set<HTMLElement>} */ = new Set();
-      for (let [name, args] of Object.entries(what)) {
-        // don't try to use the 'ignore' property as a thing to block
-        if (name != "ignore") {
-          for (let arg of args) {
-            rm[name](arg);
-          }
+/** @copyright (c) 2022 Marcell Perger @license MIT */ (function () {
+  /** @typedef {{selector: string[]; func: ((elem: HTMLElement) => boolean?)[];}} IgnoreFiltersT */
+  /** @typedef {{selector: string | null; func: (elem: Element) => any}} FuncFilterT */
+  /** @typedef {{cls?: string[], selector?: string[], func?: FuncFilterT[], ignore?: IgnoreFiltersT}} FiltersT */
+
+  function blockInDocumentWithFilters(
+    /** @type {Document} */ document,
+    /**@type {IgnoreFiltersT}*/ what,
+  ) {
+    function isContainerElem(
+      /** @type {HTMLElement} */ elem,
+    ) /** @type {boolean} */ {
+      // .tagName returns UPPERCASE for some reason
+      return ['DIV', 'SPAN'].includes(elem.tagName);
+    }
+
+    function shouldIgnore(elem) {
+      for (let s of what.ignore?.selector ?? []) {
+        if (elem.matches(s)) {
+          return true;
         }
       }
-      for (let [elem, parent] of removedElems) {
-        if (handledElems.has(elem)) {
-          continue; // already handled
+      for (let f of what.ignore?.func ?? []) {
+        if (f(elem)) {
+          return true;
         }
-        handledElems.add(elem);
-        if (!parent.isConnected) {
-          // (indirect) parent has been deleted so don't do anything here,
-          // instead go from the parent (which will also be in the Set)
-          continue;
-        }
-        if (!isContainerElem(parent)) {
-          continue; // parent might be an image or similar so don't delete
-        }
-        if (parent.hasChildNodes()) {
-          continue; // don't delete parent - info of other children would be lost
-        }
-        // no children, no info in self, so safe to delete
-        // NOTE: This will add `parent` to the end of removedElems (if not ignored) so will check again from the parent
-        rm.elem(parent);
       }
-    })({
+      return false;
+    }
+
+    var rm = {
+      elem(/** @type {HTMLElement} */ elem) {
+        if (!shouldIgnore(elem)) {
+          removedElems.add([elem, elem.parentElement]);
+          // Anti-adblock stuff might overwrite their own prototype
+          // This isn't perfect by any means but it's good enough.
+          HTMLElement.prototype.remove.call(elem);
+        }
+      },
+      list(/** @type {HTMLElement[]} */ elems) {
+        Array.from(elems).forEach((v) => rm.elem(v));
+      },
+      cls(/**@type {string} */ name) {
+        rm.list(document.getElementsByClassName(name));
+      },
+      selector(/** @type {string} */ selector) {
+        rm.list(document.querySelectorAll(selector));
+      },
+      func({ func, selector = null }) {
+        let elems =
+          selector == null
+            ? document.getElementsByClassName('*')
+            : document.querySelectorAll(selector);
+        for (let elem of elems) {
+          if (func(elem)) {
+            rm.elem(elem);
+          }
+        }
+      },
+    };
+    var /** @type {Set<[HTMLElement, HTMLElement]>} */ removedElems = new Set();
+    var handledElems /** @type {Set<HTMLElement>} */ = new Set();
+    for (let [name, args] of Object.entries(what)) {
+      // don't try to use the 'ignore' property as a thing to block
+      if (name != 'ignore') {
+        for (let arg of args) {
+          rm[name](arg);
+        }
+      }
+    }
+    for (let [elem, parent] of removedElems) {
+      if (handledElems.has(elem)) {
+        continue; // already handled
+      }
+      handledElems.add(elem);
+      if (!parent.isConnected) {
+        // (indirect) parent has been deleted so don't do anything here,
+        // instead go from the parent (which will also be in the Set)
+        continue;
+      }
+      if (!isContainerElem(parent)) {
+        continue; // parent might be an image or similar so don't delete
+      }
+      if (parent.hasChildNodes()) {
+        continue; // don't delete parent - info of other children would be lost
+      }
+      // no children, no info in self, so safe to delete
+      // NOTE: This will add `parent` to the end of removedElems (if not ignored) so will check again from the parent
+      rm.elem(parent);
+    }
+  }
+  function blockInDocument(/**@type {Document}*/ document) {
+    let filters = {
       cls: [
-        "adsbygoogle",
-        "mod_ad_container",
-        "brn-ads-box",
-        "gpt-ad",
-        "ad-box",
-        "top-ads-container",
-        "adthrive-ad",
+        'adsbygoogle',
+        'mod_ad_container',
+        'brn-ads-box',
+        'gpt-ad',
+        'ad-box',
+        'top-ads-container',
+        'adthrive-ad',
       ],
       selector: [
         '[aria-label="advertisement"]',
         '[class*="-ad "],[class*="-ad-"],[class$="-ad"],[class^="ad-"],[class^="adthrive"]',
         ':is(div,iframe)[id^="google_ads_iframe_"]',
-        "#aipPrerollContainer",
+        '#aipPrerollContainer',
         // This should really select the top one but we let the 'only contains ads' functionality handle it.
         // Yes I know its lazy, but it is more elegant than writing a whole new func filter (and more performant)
-        "span[data-ez-ph-id] span[data-ez-ph-owner-id] span.ezoicwhat",
+        'span[data-ez-ph-id] span[data-ez-ph-owner-id] span.ezoicwhat',
         // Malfunctioning overambitious adblocker-wall on Cite This For Me
-        "div#_60cc9a6b-496d-4e44-90d8-0b2947bfd3ce",
+        'div#_60cc9a6b-496d-4e44-90d8-0b2947bfd3ce',
         // hopefully not too overambitious (vm-placement is quite generic but 'placement' is used so often just for ads so it's fine)
         '.vm-placement + :has(iframe):not(:has(* + *)) iframe',
         // Dumbing of Age comic thing (error-prone, may need to change for later website versions)
-        'iframe#p_AIW8hnK, iframe#p_AIW8hnK, iframe#p_Xdy8q6J'
+        'iframe#p_AIW8hnK, iframe#p_AIW8hnK, iframe#p_Xdy8q6J',
       ],
       /** @type {{selector: string?, func: (elem: Element) => any}[]} */
       func: [
@@ -134,30 +139,30 @@
           },
         },
         {
-          selector: "div#preroll",
+          selector: 'div#preroll',
           func(elem) {
             // match div#preroll that has child div#aipBranding
             for (let c of elem.children) {
-              if (c.matches("div#aipBranding")) {
+              if (c.matches('div#aipBranding')) {
                 return true;
               }
             }
           },
         },
         {
-          selector: "html > iframe",
+          selector: 'html > iframe',
           func(/** @type {HTMLIFrameElement} */ elem) {
             // Some sanity checks not to accidenally break websites
             if (
               !(
-                elem.sandbox.contains("allow-scripts") &&
-                elem.sandbox.contains("allow-same-origin") &&
+                elem.sandbox.contains('allow-scripts') &&
+                elem.sandbox.contains('allow-same-origin') &&
                 elem.sandbox.length == 2
               )
             ) {
               return false;
             }
-            if (!elem.src.toLowerCase().includes("gdpr")) {
+            if (!elem.src.toLowerCase().includes('gdpr')) {
               // Ad iframes very often include a `?gdpr=...` in the URL
               return false;
             }
@@ -174,14 +179,14 @@
       ],
       ignore: {
         selector: [
-          "body",
-          ".ad-layout",
-          "#game-holder.game-holder-with-ad",
-          ".no-interstitial-ads",
+          'body',
+          '.ad-layout',
+          '#game-holder.game-holder-with-ad',
+          '.no-interstitial-ads',
         ],
         func: [
           (elem) => {
-            let articles = document.getElementsByTagName("article");
+            let articles = document.getElementsByTagName('article');
             for (let a of articles) {
               if (elem.contains(a)) {
                 return true; // ignore if an article descends from it
@@ -190,10 +195,24 @@
           },
         ],
       },
-    });
+    };
+    return blockInDocumentWithFilters(document, filters);
+  }
+
+  var lastRun = -1e9;
+  function block() {
+    // run every 100ms at most, but clamp other side to prevent timezone weirdness
+    // TODO: what if new mutation while still this period but not caused by use?!
+    if (lastRun < Date.now() && Date.now() < lastRun + 100) return;
+    lastRun = Date.now();
+    blockInDocument(/*global*/document);
   }
   // TODO: smarter logic would be good here to only check elements modified. But that would require
   //  a complete re-architecture of the code so may be undesirable
-  new MutationObserver(block).observe(document.body, {attributes: true, childList: true, subtree: true});
+  new MutationObserver(block).observe(document.body, {
+    attributes: true,
+    childList: true,
+    subtree: true,
+  });
   block();
 })();
